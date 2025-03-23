2b60bce4be2682c40796a8d50faa98f8c90f6205
Gerdo Germann <gerdogermann@gmail.com>
2025-03-23 20:48:17 +0200

    changes for pipeline

---
+++ backend/src/test/java/ee/ut/eba/domain/export/service/ExcelComposerServiceTest.java
@@ -0,0 +1,11 @@
+package ee.ut.eba.domain.export.service;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ExcelComposerServiceTest {
+    @Test
+    void compose() {
+        assertEquals(1, 1);
+    }
+  
+}
--- frontend/config.forge.js
+++ frontend/config.forge.js
@@ -1,19 +1,31 @@
 module.exports = {
     packagerConfig: {
-        icon: "assets/icons/mac/icon.icns",
+        icon: "assets/icons/mac/icon.icns", // macOS icon
         osxSign: false,
         osxNotarize: false
     },
     makers: [
+        // macOS DMG maker
         {
             name: "@electron-forge/maker-dmg",
             config: {
                 format: "ULFO"
             }
         },
+        // macOS ZIP maker
         {
             name: "@electron-forge/maker-zip",
             platforms: ["darwin"]
+        },
+        // Linux AppImage maker
+        {
+            name: "electron-forge-maker-appimage",
+            platforms: ["linux"]
+        },
+        // Linux deb package maker
+        {
+            name: "@electron-forge/maker-deb",
+            platforms: ["linux"]
         }
     ]
 };
